dcde643d9fa56e1a60a388cc6e0c6d9153a7adf0
asmasultana07 <ab.asma1084@gmail.com>
2025-09-01 02:39:59 +0600

    deshi deals design complete

---
+++ script/index.js
@@ -0,0 +1,11 @@
+//event add korar system
+
+//fuction ageo decleare kre rakte pari
+// function alertMessage() {
+//    alert("রান্না করার স্টিক clicked");
+// }
+
+// document.getElementById('cart-btn-stick').addEventListener('click', function(){
+//     alert("রান্না করার স্টিক clicked");
+// })
+
